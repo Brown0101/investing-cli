2e27025e0903b60aa08b9b920b6a59ba5cb1485e
Lluvian <dwayne@dontasweb.com>
2021-08-20 21:25:38 -0500

    Added code to get risk with additional data.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # https://pypi.org/project/yfinance/
 import typer
+import numpy as np
+import pandas as pd
 import yfinance as yf
-import json
+
 
 app = typer.Typer()
 
@@ -17,9 +19,17 @@ def price(stock: str):
 @app.command()
 def risk(stock: str):
     """Give the risk associated with stock, mutual fund or etf"""
-
-    # TODO: Need to implement to find sharpe ratio, alpha, beta, standard diviation, and R-Squared.
-    typer.echo(f"Stock Risk: {stock}")
+    stock_data = yf.Ticker(stock.upper()).info
+    # stock_data_dict = {
+    #     "Stock": stock.upper(),
+    #     "Industry": stock_data["industry"],
+    #     "Beta": stock_data["beta"],
+    #     "Market Cap": stock_data["marketCap"],
+    # }
+    # df = pd.DataFrame(stock_data_dict, index=[1, 2, 3, 4])
+    typer.echo(
+        f"Stock: {stock.upper()}\nIndustry: {stock_data['industry']}\nBeta {stock_data['beta']: .2f}\nMarkent Cap: {stock_data['marketCap']: .2f}\nOpen: {stock_data['open']: .2f}"
+    )
 
 
 if __name__ == "__main__":
